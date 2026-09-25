a2abe4d62c16d1b623ca341dbef615361a7f0573
kodxity <kodxity@gmail.com>
2026-09-24 23:53:35 -0400

    stride

--- src/submission.hpp
+++ src/submission.hpp
@@ -12,22 +12,26 @@ class Grid {
    private:
     std::size_t rows_;
     std::size_t cols_;
+    std::size_t stride_;
     std::vector<double> data_;
 
    public:
     Grid(std::size_t rows, std::size_t cols)
-        : rows_(rows), cols_(cols), data_(rows * cols) {}
+        : rows_(rows), cols_(cols), stride_(cols), data_(rows * stride_) {}
 
     double& operator()(std::size_t i, std::size_t j) {
-        return data_[i * cols_ + j];
+        return data_[i * stride_ + j];
     }
     double operator()(std::size_t i, std::size_t j) const {
-        return data_[i * cols_ + j];
+        return data_[i * stride_ + j];
     }
 
     std::size_t rows() const { return rows_; }
-
     std::size_t cols() const { return cols_; }
+    std::size_t stride() const { return stride_; }
+
+    double* data() { return data_.data(); }
+    const double* data() const { return data_.data(); }
 };
 
 // Apply the five-point stencil over all interior points, copying the boundary
@@ -36,11 +40,20 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid) {
     const std::size_t rows = old_grid.rows();
     const std::size_t cols = old_grid.cols();
 
+    const double* old_data = old_grid.data();
+    double* new_data = new_grid.data();
+    const std::size_t old_stride = old_grid.stride();
+    const std::size_t new_stride = new_grid.stride();
+
     for (std::size_t i = 1; i < rows - 1; i++) {
         for (std::size_t j = 1; j < cols - 1; j++) {
-            new_grid(i, j) = 0.5 * old_grid(i, j) +
-                             0.125 * (old_grid(i - 1, j) + old_grid(i + 1, j) +
-                                      old_grid(i, j - 1) + old_grid(i, j + 1));
+            const std::size_t old_index = i * old_stride + j;
+            const std::size_t new_index = i * new_stride + j;
+            new_data[new_index] =
+                0.5 * old_data[old_index] +
+                0.125 * (old_data[old_index - old_stride] +
+                         old_data[old_index + old_stride] +
+                         old_data[old_index - 1] + old_data[old_index + 1]);
         }
     }
 
